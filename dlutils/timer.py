4faf397248d4cd2ca9e2b3fd62e4fde8359d8e33
Stanislav Pidhorskyi (Podgorskiy) <stanislav@podgorskiy.com>
2019-03-04 01:32:22 -0500

    Added doc string for timer

--- dlutils/timer.py
+++ dlutils/timer.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Stanislav Pidhorskyi
+# Copyright 2017-2019 Stanislav Pidhorskyi
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,30 @@ import time
 
 
 def timer(f):
-    """Decorator for timeing method execution time"""
+    """ Decorator for timeing function (method) execution time.
+
+    After return from function will print string: ``func: <function name> took: <time in seconds> sec``.
+
+    Args:
+        f (Callable[Any]): function to decorate.
+
+    Returns:
+		Callable[Any]: Decorated function.
+
+    Example:
+
+        ::
+
+            >>> from dlutils import timer
+			>>> @timer.timer
+			... def foo(x):
+			...     for i in range(x):
+			...             pass
+			...
+			>>> foo(100000)
+			func:'foo'  took: 0.0019 sec
+
+    """
     def __wrapper(*args, **kw):
         time_start = time.time()
         result = f(*args, **kw)
